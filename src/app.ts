286270a74399155a296a8defe1bbb2e3d72b3d45
naviadev <qo2oq1020@gmail.com>
2024-07-19 00:13:51 +0900

    Feat | Socket.IO를 통한 JS Client , Express 통신 구현

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,30 @@
 import express, { Request, Response } from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
-
+import { Server as SocketIOServer } from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new SocketIOServer(server);
+
+
+io.on('connection', (socket) => {
+  console.log('online');
+
+  socket.on('disconnect', () => {
+    console.log('offline')
+  });
+
+  socket.on('test', (msg: any) => {
+    console.log(msg)
+    // io.emit('message', msg)
+  });
+
+});
+app.use(express.static(path.join(process.cwd(), './client')));
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
-})
+});
 
-server.listen(3000, ()=>{console.log('http://localhost:3000')})
+server.listen(3000, () => { console.log('http://localhost:3000') })
