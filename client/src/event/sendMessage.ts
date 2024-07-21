73504b2132468a7faa9a9e54b9ad1178f0b19e34
naviadev <qo2oq1020@gmail.com>
2024-07-21 23:55:22 +0900

    Fix | Client 이벤트 모듈화 , sendMessage 로직 수정, test.js 삭제
    1. sendMessage 내부에서 새롭게 io를 인스턴스하는 것이 아닌, 매개변수로 주입받고 로직을 진행하는 형태로 수정.

--- client/src/event/sendMessage.ts
+++ client/src/event/sendMessage.ts
@@ -1,13 +1,12 @@
-import { io, Socket } from "socket.io-client";
+import { Socket } from "socket.io-client";
 
-const sendMessage = () => {
-  const socket: Socket = io();
+const sendMessage = (socket: Socket) => {
   const messageInput = document.getElementById("messageInput") as HTMLInputElement;
   const message = messageInput.value.trim();
   if (message !== "") {
     socket.emit("test", message);
     messageInput.value = "";
   }
-}
+};
 
 export default sendMessage;
--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,22 +1,21 @@
+// client/src/index.ts
 import { io, Socket } from "socket.io-client";
 import sendMessage from "./event/sendMessage";
 
 const socket: Socket = io();
 
-//연결
 socket.on("connect", () => {
-  console.log("Connected Server");
-})
+  console.log("Connected to Server");
+});
 
-//Server로 들어온 message
 socket.on("message", (message) => {
   const messages: HTMLUListElement = document.getElementById("messages") as HTMLUListElement;
   const li: HTMLLIElement = document.createElement('li');
   li.innerHTML = message;
   messages.appendChild(li);
-})
+});
 
 window.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('submit') as HTMLButtonElement;
-  button.addEventListener('click', sendMessage);
-})
+  button.addEventListener('click', () => sendMessage(socket));
+});
